24b40bb158e54aebf5b875e8e8674b65677c5354
Mhd Ramadhan Arvin <mramadhan687@gmail.com>
2020-12-29 21:00:08 +0700

    bug

--- config/telegram.php
+++ config/telegram.php
@@ -39,7 +39,7 @@ return [
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
                 \App\Telegram\Commands\StartCommand::class,
-                \Telegram\Bot\Commands\HelpCommand::class
+                // \Telegram\Bot\Commands\HelpCommand::class
             ],
         ],
 
